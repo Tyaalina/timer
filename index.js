f82072225af804db6ba268a7ae950fc9d5eaa949
Alina <tyalinav@yandex.ru>
2023-07-30 12:46:36 +0300

    Добавлена функция для форматирования строки

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
+const format = (time) => {
+  console.log("func:", time)
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
+  return hours.toString().padStart(2, '0') + ":" + minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0')
+}
+
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
