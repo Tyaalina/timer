dd016a9dbc588dd8773e95cdf6e1eedcb80aaa03
Alina <tyalinav@yandex.ru>
2023-07-30 13:35:50 +0300

    Добавлена функция анимации таймера

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
 const format = (time) => {
-  console.log("func:", time)
   const hours = Math.floor(time / 3600);
   const minutes = Math.floor((time % 3600) / 60);
   const seconds = time % 60;
@@ -14,7 +13,26 @@ const format = (time) => {
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    let currentTime = seconds;
+
+    buttonEl.disabled = true;
+
+    // Используем функцию setInterval, которая повторяет вызов через определённый интервал времени в миллисекундах
+    let intervalId = setInterval(() => {
+      //Выводим данные в форму таймера
+      timerEl.innerHTML = format(currentTime);
+      //Уменьшаем текущее время
+      -- currentTime;
+      //Если таймер закончился
+      if (currentTime < 0) {
+        //Очищаем функцию из памяти
+        clearInterval(intervalId);
+        timerEl.innerHTML = "FINISH"
+        buttonEl.disabled = false;
+      }
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
